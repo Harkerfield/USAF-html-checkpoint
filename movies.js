a2123aed45252aa92c5e65a1a70200c835edb215
Harkerfield <hartsfieldjwh@gmail.com>
2023-01-31 11:52:42 -0900

    updated

--- movies.js
+++ movies.js
@@ -1,4 +1,19 @@
 //https://developers.themoviedb.org/3/getting-started/images
+//https://www.movieposterdb.com/
 
+/**
+ * JS to support onclick of images and navigates them using the ID to its new page.
+ */
 
-//do stuff
+var imgNavigation = function() {
+    console.log(this.id);
+
+    window.location.href = "movies/" + this.id + '.html';
+
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    var img = document.querySelector('img.movieSearch-grid-item');
+    img.addEventListener('click', imgNavigation);
+  });
+  
