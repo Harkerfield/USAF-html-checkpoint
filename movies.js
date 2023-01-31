bd70bf8a7c20769c88fedc2745767746f54d3870
Harkerfield <hartsfieldjwh@gmail.com>
2023-01-31 12:24:48 -0900

    updated click handler

--- movies.js
+++ movies.js
@@ -5,15 +5,19 @@
  * JS to support onclick of images and navigates them using the ID to its new page.
  */
 
-var imgNavigation = function() {
-    console.log(this.id);
+var imgNavigation = function () {
+  if (this.id === undefined || this.id === "" || this.id === null) {
+    alert("Sorry, that page is unavailable at the moment. Try flying Highway to the...")
 
+  } else {
+    console.log("clicked image: ", this.id);
     window.location.href = "movies/" + this.id + '.html';
-
   }
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    var img = document.querySelector('img.movieSearch-grid-item');
-    img.addEventListener('click', imgNavigation);
+}
+
+var elements = document.getElementsByClassName("movieSearch-grid-item");
+document.addEventListener("DOMContentLoaded", function () {
+  Array.from(elements).forEach(function (element) {
+    element.addEventListener('click', imgNavigation);
   });
-  
+});
